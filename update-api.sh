01e3714da2ebf4bbfd9c0ad5c6f93765147b29af
St. Michael church <45277697+stmichaelmontreal@users.noreply.github.com>
2019-03-22 00:12:48 -0400

    Update update-api.sh

--- update-api.sh
+++ update-api.sh
@@ -3,7 +3,7 @@
 cd /var/www/api
 sudo git pull origin master
 sudo rm -rf /var/www/api/node_modules
-npm install --production
+sudo npm install --production
 cd fdb
 sudo bash folders.sh
 sudo su -c "pm2 restart 0" -s /bin/sh wwwapi
