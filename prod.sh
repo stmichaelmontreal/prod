5822a98ab268864edecee1f85349a916a14445ee
sv0505 <sv0505@gmail.com>
2019-03-21 22:00:44 -0400

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -9,16 +9,18 @@ sudo apt-get install -y nginx
 sudo npm install -g pm2
 
 # api
-sudo useradd wwwapi
+sudo useradd -m -d /home/wwwapi wwwapi
+sudo passwd wwwapi
 sudo git clone https://github.com/stmichaelmontreal/api.git /var/www/api
 sudo npm install --prefix /var/www/api --production
 sudo find /var/www/api/fdb/ -exec chown wwwapi: {} \;
 sudo chmod -R u+rw,g+rw,o+rw /var/www/api/log/
 
 # pm2
-pm2 start /var/www/api/server.js
-pm2 startup
+sudo su -c "pm2 start /var/www/api/server.js" -s /bin/sh wwwapi
+# pm2 startup
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u wwwapi --hp /home/wwwapi
+sudo su -c "pm2 save" -s /bin/sh wwwapi
 
 # nginx
 sudo systemctl stop nginx
