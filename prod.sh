3f3f54f15931685f2604dae040d01034d57faec9
Sergii Lutchyn <stmichaelmontreal@gmail.com>
2019-04-02 23:06:34 -0400

    install

--- prod.sh
+++ prod.sh
@@ -7,10 +7,10 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y unzip
 sudo apt-get install -y nginx
 sudo apt-get install -y mysql-server
+
 sudo mysql_secure_installation
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'PutHerePassword'"
 sudo mysql -e "FLUSH PRIVILEGES"
-
 
 sudo npm install -g pm2
 
@@ -19,13 +19,12 @@ sudo useradd -m -d /home/wwwapi wwwapi
 sudo passwd wwwapi
 sudo git clone https://github.com/stmichaelmontreal/api.git /var/www/api
 sudo npm install --prefix /var/www/api --production
-sudo find /var/www/api/fdb/ -exec chown wwwapi: {} \;
-sudo find /var/www/api/log/ -exec chown wwwapi: {} \;
-sudo chmod -R u=rwx,g=rx,o=rx /var/www/api/fdb/
-sudo chmod -R u=rwx,g=rx,o=rx /var/www/api/log/
+sudo find /var/www/api/ -exec chown wwwapi: {} \;
+sudo chmod -R u=rwx,g=rx,o=rx /var/www/api/
 
 # pm2
-sudo su -c "pm2 start /var/www/api/server.js" -s /bin/sh wwwapi
+cd /var/www/api/
+sudo su -c "pm2 start server.js" -s /bin/sh wwwapi
 # pm2 startup
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u wwwapi --hp /home/wwwapi
 sudo su -c "pm2 save" -s /bin/sh wwwapi
